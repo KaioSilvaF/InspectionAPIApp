8241b28dc3e199afcabbb9dd9a05b2c4a58b6149
KaioSilvaF <ksferreira7@hotmail.com>
2022-05-04 11:48:27 -0300

    First Commit

---
+++ InspectionAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option) { }
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; } 
+        public DbSet<Status> Statuses { get; set; }
+        
+    }
+}
---
+++ InspectionAPI/Inspection.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class Inspection
+    {
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Status{ get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public String Comments { get; set; } = string.Empty;
+
+        public int InspectionTypeId { get; set; }
+
+        // ? mark means it can be null
+        public InspectionType? InspectionType { get; set; }
+
+    }
+}
